eaba1ed707141a841b33b96df21727b4daa91cea
Edwin Hartnell <ehartnell3@gatech.edu>
2023-07-02 14:42:25 -0400

    Generating movie of camera images

--- PID_Wall_Tap.py
+++ PID_Wall_Tap.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('/home/pi/TurboPi/')
 import cv2
 import os
+import shutil
 import time
 import math
 import signal
@@ -15,6 +16,7 @@ import pandas as pd
 import HiwonderSDK.Sonar as Sonar
 import HiwonderSDK.Board as Board
 import HiwonderSDK.mecanum as mecanum
+import imageio.v3 as iio
 
 ##################################################
 # PID Wall Tap
@@ -23,214 +25,139 @@ import HiwonderSDK.mecanum as mecanum
 # using a PID controller that will cause the robot to
 # gently tap the wall before stopping.
 #
+# The robot will turn its camera to the right and take
+# a sequence of photos as it moves. When it stops the
+# robot will stitch the photos together into a gif movie.
+#
 ##################################################
 
-if sys.version_info.major == 2:
+# Check the version of Python running this code
+if sys.version_info.major <= 2:
     print('Please run this program with python3!')
     sys.exit(0)
 
-car = mecanum.MecanumChassis()
-
 DEBUG = True
-MAX_DISTANCE_THRESHOLD = 100.0 # cm - if greater distance away from the wall than this run at full speed
+IMAGES_DIRECTORY = './images'
+MOVIES_DIRECTORY = './movies'
+IMAGE_TEXT_SIZE = 12
+IMAGE_TEXT_COLOR = (0, 255, 255)
+
+SERVO_DEFAULT_POSITION = 1500 # The default 90-degree position for both camera servos - may have been adjusted
+SERVO_HARD_RIGHT_POSITION = 500 # Pulse Width = Angle-in-degrees * 11.1 + 500 - turn the camera servo hard-right
+SERVO_HARD_LEFT_POSITION = 2500 # Turn the camera servo hard-left = 180 * 11.1 + 500
+MAX_DISTANCE_THRESHOLD = 100.0 # cm - if greater distance_mean away from the wall than this run at full speed
 MAX_SPEED = 100.0 # mm/second - the maximum speed the robot can travel
-MIN_DISTANCE_THRESHOLD = 5.0 # cm - if distance to the wall is closer than this then stop the robot
+MIN_DISTANCE_THRESHOLD = 5.0 # cm - if distance_mean to the wall is closer than this then stop the robot
 MIN_SPEED = 25.0 # mm/second - the minimum speed the robot can travel if not stopping
-distance = MAX_DISTANCE_THRESHOLD
+PID_PROPORTIONAL_COEFFICIENT = 2.0 # Multiply the distance_mean from the wall by this coefficient
+
+servo1_default_position = SERVO_DEFAULT_POSITION # These defaults will be updated by the fine tuning found in the YAML file
+servo2_default_position = SERVO_DEFAULT_POSITION
+distance_mean = MAX_DISTANCE_THRESHOLD # cm - the mean distance to the wall
 distance_last = MAX_DISTANCE_THRESHOLD # cm - distances can only decrease as the wall approaches
 distance_list = []
-IMAGE_DIRECTORY = './images'
 speed = MAX_SPEED
 
-PID_PROPORTIONAL_COEFFICIENT = 2.0 # Multiply the distance from the wall by this coefficient
+robot = mecanum.MecanumChassis()
+sonar = Sonar.Sonar()
+camera = Camera.Camera()
 
-HWSONAR = None
-stopMotor = True
-__isRunning = False
-servo1_default_position = 1500
-servo2_default_position = 1500
-
-TextSize = 12
-TextColor = (0, 255, 255)
+stop_motor = True
+is_running = False
 
 # Initialize the robot's hardware
-def initMove():
-    global DEBUG
-    global servo1_default_position
-    global servo2_default_position
+def initialize():
+    if DEBUG: print("[initialize]")
 
-    if DEBUG: print("[initMove]")
-    if DEBUG: print(f'[initMove] car.set_velocity(0,90,0)')
-    car.set_velocity(0,90,0) # velocity (mm/s), direction (0-360 with 90=straight-ahead), angular rate (speed at which chassis rotates between -2 and 2)
+    # PNG / JPG images go to the images director
+    # GIF is created from the plots and saved in the movies directory
+    if not os.path.exists(IMAGES_DIRECTORY): os.makedirs(IMAGES_DIRECTORY)
+    if not os.path.exists(MOVIES_DIRECTORY): os.makedirs(MOVIES_DIRECTORY)
+
+    # Clear out the old images from the images directory
+    folder = IMAGES_DIRECTORY
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as exception:
+            print(f'Failed to delete the file {file_path}. Reason: {exception}')
+
+    # Clear out the old gif files from the movies directory
+    folder = MOVIES_DIRECTORY
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as exception:
+            print(f'Failed to delete the file {file_path}. Reason: {exception}')
+
+    # Stop the robot if it was moving
+    if DEBUG: print(f'[initialize] robot.set_velocity(0, 90, 0)')
+    robot.set_velocity(0, 90, 0) # velocity (mm/s), direction (0-360 with 90=straight-ahead), angular rate (speed at which chassis rotates between -2 and 2)
+
+    # Get the default camera servo positions
     servo_data = yaml_handle.get_yaml_data(yaml_handle.servo_file_path) # Calibrated servo positions: {'servo1': 1440, 'servo2': 1440}
     servo1_default_position = servo_data['servo1']
     servo2_default_position = servo_data['servo2']
-#    if DEBUG: print(f'[initMove] Board.setPWMServoPulse(1, {servo1_default_position}, 1000)')
-#    Board.setPWMServoPulse(1, servo1_default_position, 1000) # Camera Up/Down
-#    time.sleep(1) # Move 1000 ms = 1 second
-#    if DEBUG: print(f'[initMove] Board.setPWMServoPulse(2, {servo2_default_position}, 1000)')
-#    Board.setPWMServoPulse(2, servo2_default_position, 1000) # Camera Left/Right
-#    time.sleep(1) # Move 1000 ms = 1 second
 
-    if DEBUG: print(f'[initMove] Board.setBuzzer(1)')
-    Board.setBuzzer(1) # Turn on the Buzzer
-    time.sleep(0.1) # Leave the Buzzer on for a short blip
-    if DEBUG: print(f'[initMove] Board.setBuzzer(0)')
-    Board.setBuzzer(0) # Turn off the Buzzer
-
-    # Pulse Width = Angle-in-degrees * 11.1 + 500
-    if DEBUG: print(f'[initMove] Board.setPWMServoPulse(2, 500, 1000)')
-    Board.setPWMServoPulse(2, 500, 1000) # 500 = 0-degrees * 11.1 + 500 = point camera hard-right
-    time.sleep(1) # Move 1000 ms = 1 second
-    # Board.setPWMServoPulse(2, 1500, 1000) # 1500 = 90-degrees * 11.1 + 500 = point camera straight ahead
-    # Board.setPWMServoPulse(2, 2500, 1000) # 2500 = 180-degrees * 11.1 + 500 = point camera hard-left
-
-    if DEBUG: print(f'[initMove] Board.setBuzzer(1)')
-    Board.setBuzzer(1) # Turn on the Buzzer
-    time.sleep(1.0) # Leave the Buzzer on for a long blip
-    if DEBUG: print(f'[initMove] Board.setBuzzer(0)')
-    Board.setBuzzer(0) # Turn off the Buzzer
-
-# Reset the robot's runtime parameters
-def reset():
-    global DEBUG
-    global MAX_DISTANCE_THRESHOLD # cm - if greater distance away from the wall than this run at full speed
-    global MAX_SPEED # mm/second - the maximum speed the robot can travel
-    global speed
-    global distance
-    global stopMotor
-    global __isRunning
-    
-    if DEBUG: print("[reset]")
-    distance = MAX_DISTANCE_THRESHOLD
-    speed = MAX_SPEED
-    stopMotor = True
-    __isRunning = False
-    
-# App Initialization
-def init():
-    global DEBUG
-    if DEBUG: print("[init]")
-    initMove()
-    reset()
-    
-# App Start
-__isRunning = False
-def start():
-    global DEBUG
-    global __isRunning
-    global stopMotor
-    
-    if DEBUG: print("[start]")
-    stopMotor = True
-    __isRunning = True
-
-# App Stop
-def stop():
-    global DEBUG
-    global servo1_default_position
-    global servo2_default_position
-    global __isRunning
-    
-    if DEBUG: print("[stop]")
-    Board.setBuzzer(1) # Turn on the Buzzer
-    time.sleep(0.1) # Leave the Buzzer on for a short blip
-    if DEBUG: print(f'[stop] Board.setBuzzer(0)')
-    Board.setBuzzer(0) # Turn off the Buzzer
-    
-    __isRunning = False
-    if DEBUG: print(f'[stop] car.set_velocity(0,90,0)')
-    car.set_velocity(0,90,0)
+    # Prepare to move the camera into position to take a sequence of images
+    if DEBUG: print(f'[initialize] Board.setPWMServoPulse(2, {SERVO_HARD_RIGHT_POSITION}, 1000)')
+    Board.setPWMServoPulse(2, SERVO_HARD_RIGHT_POSITION, 1000) # 500 = 0-degrees * 11.1 + 500 = point camera hard-right
     time.sleep(0.3)
-    if DEBUG: print(f'[stop] car.set_velocity(0,90,0)')
-    car.set_velocity(0,90,0)
-#    if DEBUG: print(f'[stop] Board.setPWMServoPulse(1, {servo1_default_position}, 1000)')
-#    Board.setPWMServoPulse(1, servo1_default_position, 1000) # Camera Up/Down
-#    time.sleep(1) # Move 1000 ms = 1 second
-    if DEBUG: print(f'[stop] Board.setPWMServoPulse(2, {servo2_default_position}, 1000)')
-    Board.setPWMServoPulse(2, servo2_default_position, 1000) # Camera Left/Right
+    if DEBUG: print(f'[initialize] Board.setPWMServoPulse(2, {SERVO_HARD_RIGHT_POSITION}, 1000)')
+    Board.setPWMServoPulse(2, SERVO_HARD_RIGHT_POSITION, 1000) # 500 = 0-degrees * 11.1 + 500 = point camera hard-right
     time.sleep(1) # Move 1000 ms = 1 second
 
-    if DEBUG: print(f'[stop] Board.setBuzzer(1)')
-    Board.setBuzzer(1) # Turn on the Buzzer
-    time.sleep(1.0) # Leave the Buzzer on for a long blip
-    if DEBUG: print(f'[stop] Board.setBuzzer(0)')
-    Board.setBuzzer(0) # Turn off the Buzzer
+    # Turn the camera on
+    if DEBUG: print('[initialize] camera.camera_open(correction=True)')
+    camera.camera_open(correction=True) # Take an image from the camera
+    time.sleep(1.0) # Allow time for the camera to turn on
 
-# App Exit
-def exit():
-    global DEBUG
-    global __isRunning
-    __isRunning = False
+    # Turn the sonar colors on
+    if DEBUG: print(f'[initialize] sonar.setPixelColor(0, Board.PixelColor(255, 0, 0))')
+    sonar.setPixelColor(0, Board.PixelColor(255, 0, 0))
+    if DEBUG: print(f'[initialize] sonar.setPixelColor(1, Board.PixelColor(255, 0, 0))')
+    sonar.setPixelColor(1, Board.PixelColor(255, 0, 0))
 
-    if DEBUG: print("[exit]")
-    if DEBUG: print(f'[exit] car.set_velocity(0,90,0)')
-    car.set_velocity(0,90,0)
-    time.sleep(0.3)
-    if DEBUG: print(f'[exit] car.set_velocity(0,90,0)')
-    car.set_velocity(0,90,0)
-    if DEBUG: print(f'[exit] HWSONAR.setPixelColor(0, Board.PixelColor(0, 0, 0))')
-    HWSONAR.setPixelColor(0, Board.PixelColor(0, 0, 0))
-    if DEBUG: print(f'[exit] HWSONAR.setPixelColor(1, Board.PixelColor(0, 0, 0))')
-    HWSONAR.setPixelColor(1, Board.PixelColor(0, 0, 0))
-    print("[exit] PID Wall Tap Exit")
-
-# Set Speed from App
-def setSpeed(args):
-    global DEBUG
-    global speed
-
-    if DEBUG: print("[exit]")
-    speed = int(args[0])
-    return (True, ())
-
-# Move the robot each iteration
+# Move the robot each iteration - this routine runs independently in a thread
 def move():
-    global DEBUG
-    global MAX_DISTANCE_THRESHOLD # cm - if greater distance away from the wall than this run at full speed
-    global MAX_SPEED # mm / second - the maximum speed the robot can travel
-    global MIN_DISTANCE_THRESHOLD # cm - if distance to the wall is closer than this then stop the robot
-    global MIN_SPEED # mm/second - the minimum speed the robot can travel if not stopping
-    global PID_PROPORTIONAL_COEFFICIENT # Multiply the distance from the wall by this coefficient
-
-    global speed
-    global distance
-    global stopMotor
-    global __isRunning
+    global is_running
+    global distance_mean
 
     if DEBUG: print("[move]")
     while True:
-        if __isRunning:
-            if math.isnan(distance):
+        if is_running:
+            if math.isnan(distance_mean):
                 continue
-            if distance > MAX_DISTANCE_THRESHOLD:
+            if distance_mean > MAX_DISTANCE_THRESHOLD:
                 speed = MAX_SPEED
-            elif distance <= MIN_DISTANCE_THRESHOLD:
+            elif distance_mean <= MIN_DISTANCE_THRESHOLD:
                 forward = False
-                stopMotor = True
-                __isRunning = False
+                stop_motor = True
+                is_running = False
                 speed = 0.0
             else:
-                speed = min( max(distance * PID_PROPORTIONAL_COEFFICIENT, MIN_SPEED), MAX_SPEED )
+                speed = min( max(distance_mean * PID_PROPORTIONAL_COEFFICIENT, MIN_SPEED), MAX_SPEED )
         else:
             speed = 0.0
-        if False and DEBUG: print(f'[move] car.set_velocity({speed:.1f}, 90, 0)')
-        car.set_velocity(speed, 90, 0)
- 
-# Instantiate a thread to keep the robot moving
-th = threading.Thread(target=move)
-th.setDaemon(True)
-th.start()
+        if False and DEBUG: print(f'[move] robot.set_velocity({speed:.1f}, 90, 0)')
+        robot.set_velocity(speed, 90, 0)
 
-# Calculate the distance from the robot to the wall
-def run(img):
-    global DEBUG
-    global HWSONAR
-    global distance
+# Calculate the distance_mean from the robot to the wall
+def run(camera_image):
+    global distance_mean
+    global distance_last
     global distance_list
-    global distance_last # cm - distances can only decrease as the wall approaches
-    
-    distance_sensor = HWSONAR.getDistance() / 10.0 # Maximum sensor accuracy = 40 cm
+
+    # Get the raw distance measurement from the sensor
+    distance_sensor = sonar.getDistance() / 10.0 # Maximum sensor accuracy = 40 cm
     if False and DEBUG: print(f'[run] distance_sensor={distance_sensor}')
 
     # Ignore bad sensor measurements if the wall is getting further away 
@@ -243,50 +170,116 @@ def run(img):
         std = dataframe_.std()
 
         data_clean = dataframe[np.abs(dataframe - mu) <= std]
-        distance = data_clean.mean()[0]
+        distance_mean = data_clean.mean()[0]
 
         if len(distance_list) >= 5:
             distance_list.remove(distance_list[0])
 
-    return cv2.putText(img, "Dist:%.1fcm"%distance, (30, 480-30), cv2.FONT_HERSHEY_SIMPLEX, 1.2, TextColor, 2)  # Update the camera image
+    return cv2.putText(camera_image, "Dist:%.1fcm"%distance_mean, (30, 480-30), cv2.FONT_HERSHEY_SIMPLEX, 1.2, IMAGE_TEXT_COLOR, 2)  # Update the camera image
 
+# Stop the robot and restore defaults
+def stop():
+    if DEBUG: print("[stop]")
+    is_running = False
 
-# Manually stop the robot from the App
-def manual_stop(signum, frame):
-    global DEBUG
-    global __isRunning
-    
-    print('[manual_stop]')
-    __isRunning = False
-    if DEBUG: print(f'[manual_stop] car.set_velocity(0,90,0)')
-    car.set_velocity(0,90,0)
+    # Stop the robot from moving - stopping twice was part of the original code
+    if DEBUG: print(f'[stop] robot.set_velocity(0, 90, 0)')
+    robot.set_velocity(0, 90, 0)
+    time.sleep(0.3)
+    if DEBUG: print(f'[stop] robot.set_velocity(0, 90, 0)')
+    robot.set_velocity(0, 90, 0)
 
+    # Restore the camera to the default position
+    if DEBUG: print(f'[stop] Board.setPWMServoPulse(2, {servo2_default_position}, 1000)')
+    Board.setPWMServoPulse(2, servo2_default_position, 1000) # Camera Left/Right
+    time.sleep(0.3)
+    if DEBUG: print(f'[stop] Board.setPWMServoPulse(2, {servo2_default_position}, 1000)')
+    Board.setPWMServoPulse(2, servo2_default_position, 1000) # Camera Left/Right
+    time.sleep(1) # Move 1000 ms = 1 second
+
+    # Turn off the camera
+    camera.camera_close()
+
+    # Switch off the sonar lights
+    if DEBUG: print(f'[stop] sonar.setPixelColor(0, Board.PixelColor(0, 0, 0))')
+    sonar.setPixelColor(0, Board.PixelColor(0, 0, 0))
+    if DEBUG: print(f'[stop] sonar.setPixelColor(1, Board.PixelColor(0, 0, 0))')
+    sonar.setPixelColor(1, Board.PixelColor(0, 0, 0))
+
+# Main routine
 if __name__ == '__main__':
     if DEBUG: print('[main]')
-    init()
-    HWSONAR = Sonar.Sonar()
-    camera = Camera.Camera()
-    if DEBUG: print('[main] camera.camera_open(correction=True)')
-    camera.camera_open(correction=True) # Take an image from the camera
-    time.sleep(1.0) # Allow time for the camera to turn on
-    signal.signal(signal.SIGINT, manual_stop)
-    start()
+
+    # Announce the initialization of the robot
+    if DEBUG: print(f'[main] Board.setBuzzer(1)')
+    Board.setBuzzer(1) # Turn on the Buzzer
+    time.sleep(0.1) # Leave the Buzzer on for a short blip
+    if DEBUG: print(f'[main] Board.setBuzzer(0)')
+    Board.setBuzzer(0) # Turn off the Buzzer
+
+    # Initialize the robot
+    initialize()
+    signal.signal(signal.SIGINT, stop)
+
+    # Announce the start of motion
+    if DEBUG: print(f'[main] Board.setBuzzer(1)')
+    Board.setBuzzer(1) # Turn on the Buzzer
+    time.sleep(1.0) # Leave the Buzzer on for a long blip
+    if DEBUG: print(f'[main] Board.setBuzzer(0)')
+    Board.setBuzzer(0) # Turn off the Buzzer
+
+    # Start the robot running
+    stop_motor = True
+    is_running = True
+
+    # Instantiate a thread to keep the robot moving
+    move_thread = threading.Thread(target=move)
+    move_thread.setDaemon(True)
+    move_thread.start()
+
+    # Collect images while the robot is running
     image_id = 1
-    while __isRunning:
-        img = camera.frame
-        if img is not None:
-            frame = img.copy() # Copy the image from the camera
+    image_filenames = []
+    while is_running:
+        camera_image = camera.frame
+        if camera_image is not None:
+            frame = camera_image.copy() # Copy the image from the camera
             Frame = run(frame)  
             frame_resize = cv2.resize(Frame, (320, 240)) # Resize the image down to 320*240
             cv2.imshow('frame', frame_resize)
-            cv2.imwrite(os.path.join(IMAGE_DIRECTORY, f'{image_id:03d}.jpg'), frame)
+            image_filename = os.path.join(IMAGES_DIRECTORY, f'{image_id:03d}.jpg')
+            cv2.imwrite(image_filename, frame) # Save the image
+            image_filenames.append(image_filename)
             image_id += 1
             key = cv2.waitKey(1)
             if key == 27:
                 break
         else:
             time.sleep(0.01)
-    camera.camera_close()
-    cv2.destroyAllWindows()
+
+    # Announce the stopping of motion
+    if DEBUG: print(f'[main] Board.setBuzzer(1)')
+    Board.setBuzzer(1) # Turn on the Buzzer
+    time.sleep(0.1) # Leave the Buzzer on for a long blip
+    if DEBUG: print(f'[main] Board.setBuzzer(0)')
+    Board.setBuzzer(0) # Turn off the Buzzer
+
+    # Stop the robot and close the camera windows
     stop()
+    cv2.destroyAllWindows()
+
+    # Create Gif Animation - Heatmap Actions
+    if DEBUG: print(f'[main] Writing: {MOVIES_DIRECTORY}/camera_movie.gif') 
+    camera_images = []
+    for filename in image_filenames:
+        camera_images.append(iio.imread(filename))
+    iio.imwrite(f'{MOVIES_DIRECTORY}/camera_movie.gif', camera_images, loop=0, duration=100) # duration(in ms): `fps=50` == `duration=20` (1000 * 1/50)
+
+    # Announce the exit of main
+    if DEBUG: print(f'[main] Board.setBuzzer(1)')
+    Board.setBuzzer(1) # Turn on the Buzzer
+    time.sleep(1.0) # Leave the Buzzer on for a long blip
+    if DEBUG: print(f'[main] Board.setBuzzer(0)')
+    Board.setBuzzer(0) # Turn off the Buzzer
+
     if DEBUG: print('[main] Done.')
